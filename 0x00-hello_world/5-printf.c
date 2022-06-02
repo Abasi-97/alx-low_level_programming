6748dc967ee96f6d68427112671336c2bc64dbc2
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-02 12:31:36 -0700

    newfile

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,19 +2,15 @@
  * File: 5-printf.c
  * author: Joshua Adewale
  */
-
 #include <stdio.h>
-
 /**
  * main function will print "with proper grammar, but the outcome is a piece of art, 
  * 			     followed by a white space and a newline".
  *
- * function will return zero as always.
+ * Return: always 0.
  *
  */
-
 int main(void) 
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
