b1de2794461101cb66ef645a7bdd245675e55857
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-02 13:35:52 -0700

    newfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
 /**
  * File: 4-puts.c
- *
  * Auth: Joshua Adewale
  */
 #include<stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual
- * puzzle, followed by a new line".
- *
+ * main - prints "Programming is like building a multilingual
+ * 		puzzle, followed by a newline".
  * Return: Always 0.
- *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("\"Programming is like building a multilingual puzzle,\n");
+	return 0;
 }
