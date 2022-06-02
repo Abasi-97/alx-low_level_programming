72db99c51fe7abe3bfe045c29e3e4eabc92f5386
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-02 12:18:04 -0700

    newfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main function will print "programming is like building a multilingual puzzle, followed by a new line".
+ *
+ * Return will be zero.
+ */
 int main(void) 
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main function will print "with proper grammar, but the outcome is a piece of art, followed by a white space and a newline".
+ *
+ * function will return zero as always.
+ *
+ */
+
 int main(void) 
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
-	
 	return (0);
 }
