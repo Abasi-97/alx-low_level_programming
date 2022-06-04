bfbf0d1cb2642ac8919d9e0776ba981a848f9b32
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-03 22:53:12 -0700

    newfile

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,10 +4,11 @@
  *
  * Return: Always zero (SUCCESS)
  */
-int main()
+int main(void)
 {
 	int num;
-	for (num = 0 ; num <=9 ; num++)
+
+	for (num = 0 ; num <= 9 ; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
