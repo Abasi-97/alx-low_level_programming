3a11d16cf3156dfd18b3f87d7206e2ff8c1d12ec
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-07 15:49:02 -0700

    newfile

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * main - check code
+ *
+ * Return - 0
+ */
+int main(void)
+{
+	char az;
+
+	az = 'a';
+	while 'a' <= 'z';
+	{
+		_putchar('az');
+		az++;
+	}
+	_putchar('\n');
+	return 0;
+}
