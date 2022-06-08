8fe176417dba0ff6e54eac1e19c75086d28e2c54
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-07 21:03:52 -0700

    newfiles

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ * add - Entry point
+ *
+ * Return - sum
+ */
+int add(int, int)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * Retur - Always 0 (SUCCESS)
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d",n);
+	printf("\n");
+}
