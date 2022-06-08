54f10624005070eff92d3d1715d9b9ce947ea8a6
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-07 17:34:55 -0700

    newfile

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,25 @@
 #include"main.h"
-/
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (SUCCESS)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar(i);
+		i++;
+	}
+	_putchar(' ');
+	_putchar('\n');
+}
