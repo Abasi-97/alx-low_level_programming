017b17b53d58ae445fbf7dd589a7838cbcc1767c
Joshua Adewale <josh.jobadewale@gmail.com>
2022-06-14 00:47:07 +0100

    newfile

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,17 @@
 #include "main.h"
 
 /**
- * rev_string - reverse a string
- * @s: input string
- *
- * Description: reverses a string.
- * Return: Always (0)
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
  */
 
-void rev_string(char *s)
+void puts2(char *str)
 {
-	int left, i, temp;
+	int i;
 
-	left = 0;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (i > left)
-	{
-		temp = s[i];
-		s[i--] = s[left];
-		s[left++] = temp;
-	}
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
+
